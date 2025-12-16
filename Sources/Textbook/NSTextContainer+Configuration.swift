278392be6c33df306925512a243bcc339b39a53c
Matt <85322+mattmassicotte@users.noreply.github.com>
2025-12-16 14:41:12 -0500

    TK2 container default

---
+++ Sources/Textbook/NSTextContainer+Configuration.swift
@@ -0,0 +1,19 @@
+#if os(macOS) && !targetEnvironment(macCatalyst)
+import AppKit
+#elseif os(iOS)
+import UIKit
+#endif
+
+@available(macOS 12.0, *)
+extension NSTextContainer {
+	public static var defaultTextKit2Container: NSTextContainer {
+		let textContainer = NSTextContainer(size: CGSize(width: 1000.0, height: 1.0e7))
+		let textContentManager = NSTextContentStorage()
+		let textLayoutManager = NSTextLayoutManager()
+
+		textLayoutManager.textContainer = textContainer
+		textContentManager.addTextLayoutManager(textLayoutManager)
+
+		return textContainer
+	}
+}
